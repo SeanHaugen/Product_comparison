59322e997fe38d74e2d6ce032e910ac6a9e067ca
SeanHaugen <sean.haugen560@gmail.com>
2023-02-09 08:53:46 -0600

    Searchbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RetractorCompare from './Components/RetractorCompare';
 import Header from './Components/Header';
 import About from './Components/About';
 import TentsCompare from './Components/TentsComparison';
 import './App.css';
 import { BrowserRouter as Router, Routes,  Route } from 'react-router-dom';
+import SearchBar from './Components/SearchBar';
+
 
 function App () {
 
+    const [ filteredProducts, setFilteredProducts ] = useState([])
+
+    const handleSubmit = (input1, input2) => {
+        console.log('do a search with ', input1, 'and', input2)
+        setFilteredProducts(
+            
+        )
+    }
+
 
     return (
-    <Router>
         <div>
-            <Header />
-            <Routes>
-                <Route path='/About' element={<About />} />
-                <Route path='/RetractorCompare'element={ <RetractorCompare />} />
-                <Route path='/TentsCompare' element={<TentsCompare />} />
-            </Routes>
-
-            
+            <Router>
+                <Header />
+                <SearchBar onSubmit={handleSubmit} />
+                <Routes>
+                    <Route path='/About' element={<About />} />
+                    <Route path='/RetractorCompare'element={ <RetractorCompare />} />
+                    <Route path='/TentsCompare' element={<TentsCompare />} />
+                </Routes>
+            </Router>
         </div>
-    </Router>
+
 
     )
 }
